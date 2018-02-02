338bda4bba37bc9184de58b805f062509978d6e1
Connor <cmcclaran62@gmail.com>
2018-02-01 17:39:56 -0700

    first test

--- mth4490hw1.py
+++ mth4490hw1.py
@@ -1,3 +1,25 @@
 #Connor McClaran
 #MTH4490 HW1
 
+#y'(t) = 4t - 2y(t) d'Alembert's equation
+
+def f(t,w):
+	return 4*t - 2*w
+	
+def Euler(f,a,b,y0,n):
+        h = (b-a)/float(n)
+        x = a
+        y = y0
+        for i in range(n):
+                y += h*f(x,y)
+                x += h
+        return y
+
+#Test at y0 = 0
+
+print(Euler(f,0,1,0,4))
+
+
+
+
+        
