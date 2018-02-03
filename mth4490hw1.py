fa8a7631f39a902da39ef81d49894d3c56c999b5
Connor <cmcclaran62@gmail.com>
2018-02-02 18:38:55 -0700

    modify trap to use arrays

--- mth4490hw1.py
+++ mth4490hw1.py
@@ -1,6 +1,10 @@
 #Connor McClaran
 #MTH4490 HW1
+
+
 import math
+import numpy as np
+import matplotlib as pyplot
 
 #y'(t) = 4t - 2y(t) d'Alembert's equation
 
@@ -13,7 +17,7 @@ def g(t):
 
 def f(t,w):
 	return 4*t - 2*w
-'''	
+	
 def Euler(f,a,b,y0,n):
         
         h = (b-a)/float(n)
@@ -25,6 +29,40 @@ def Euler(f,a,b,y0,n):
                 x += h
         return y
 
+
+
+def trap(f,a,b,y0,n):
+        h = (b-a) / float(n)
+        
+        t = [a]
+        w = [y0]
+        for i in range(n):
+                print(w[i])
+                w.append(w[i] + (h/2)*(f(t[i],w[i])+ f((t[i]+h), w[i]+ h*(f(t[i],w[i])))))
+                t.append(t[i] + h)
+
+        return w,t
+
+
+
+
+
+
+#print(trap(f,0,1,0,4))
+
+# y0 = 1
+
+def h(t,w):
+        return 1 - w**2 + (t*0)
+
+
+#test failure
+print(trap(h,0,1,1,10))
+
+
+
+
+'''
 #Test genral solution
 
 print(g(1))
@@ -39,18 +77,6 @@ print(Euler(f,0,1,0,4))
 
 #print(Euler(f,0,1,0,50))
 '''
-def trap(f,a,b,y0,n):
-        h = (b-a) / float(n)
-        ti = a
-        wi = y0
-        for i in range(n):
-                print(wi)
-                wi = wi + (h/2)*(f(ti,wi)+ f((ti+h), wi+ h*(f(ti,wi))))
-                ti = ti + h
-
-        return wi
-
-print(trap(f,0,1,0,4))
 
 
         
