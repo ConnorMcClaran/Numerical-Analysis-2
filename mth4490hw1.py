8354fb910e33a0cd0643df8eae55d2c580895876
Connor <cmcclaran62@gmail.com>
2018-02-03 19:28:52 -0700

    closer to error graph create error array size 500

--- mth4490hw1.py
+++ mth4490hw1.py
@@ -18,9 +18,9 @@ def g(t):
 def f(t,w):
 	return 4*t - 2*w
 	
-def Euler(f,g,a,b,y0,n):
+def Euler(f,g,a,b,y0,n,h):
         g = [g(a)]
-        h = (b-a)/float(n)
+        
         t = [a]
         w = [y0]
         err = [0]
@@ -29,8 +29,9 @@ def Euler(f,g,a,b,y0,n):
                 g.append(g(t[i]))
                 w.append(w[i] + h*f(t[i],w[i]))
                 t.append(t[i] + h)
-                err.append(g[i] - w[i])
-        return w,t,err,g
+
+        err.append(g[i] - w[i])
+        return err
 
 
 
@@ -47,17 +48,46 @@ def trap(f,a,b,y0,n):
         return w,t
 
 def step(k):
-        return 0.1 * 2^(-k)
+        return 0.1 * 2**(-k)
+
+
+x = np.linspace(0,5,num = 500)
+print(x)
+
+def generateh(x,n):
+        h = [0]
+        for i in range (n):
+                
+                h.append( step(x[i]))
+
+        return h
+
+h = generateh(x,500)
 
 
 
+#for every h[i] I need a err[i] that corresponds with that h-value, 4 iterations
+
+def generr(h,n,f,g):
+        err = [0]
+        for i in range(n):
+                err.append(Euler(f,g,0,1,0,4,h[i])
+
+        return err
+
+print(generr(h,500,f,g))
+                        
+                           
+        
+
+
+        
 
 
 
-
-
-#print(trap(f,0,1,0,4))
 '''
+print(trap(f,0,1,0,4))
+
 # y0 = 1
 
 def h(t,w):
@@ -77,7 +107,7 @@ print(g(1))
 
 #Test Euler at y0 = 0 with 4  interations
 '''
-print(Euler(f,g,0,1,0,4))
+#print(Euler(f,g,0,1,0,4))
 '''
 #print(Euler(f,0,1,0,10))
 
