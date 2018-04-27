b2377dae020875e11fac6b9d53ebdb0d1ec87284
Connor <connormcclaran@gmail.com>
2018-04-26 23:41:20 -0600

    Add mersene prime

--- RandomNumberGenerators.py
+++ RandomNumberGenerators.py
@@ -19,17 +19,18 @@ x = random.getstate()
 
 #psuedo random number (mod 31)
 
-def mod31(seed,n):
+def mod31(seed,A,b,m,n):
     x = [seed]
     u = []
+    m = float(m)
     for i in range(n):
-        x.append((13*x[i])%31)
-        u.append(x[i]/31.0)
+        x.append((A*x[i])%m)
+        u.append(x[i]/m)
     return u	
     
 
 y31 = np.zeros(30)
-y31 = mod31(3,30)
+y31 = mod31(3,13,0,31.0,30)
 print(y31)
 
 
@@ -53,17 +54,26 @@ def MonteCarloType1(u):
 # x = MonteCarloType1(y31)     
 #print(x)  
 
-
-n = np.arange(30)
-
-                                                                      
+## plot - simple psuedo random generator (mod31)##
+n = np.arange(30)                                                                      
 fig,dx = pyplot.subplots()
 pyplot.title('simple psuedo random scatter plot')
 pyplot.scatter(n,y31)
+pyplot.xlabel('x')
+pyplot.ylabel('y')
+dx.legend()
+#################################################
+pyplot.show()
 
+#Mersenne Prime
+
+m = mod31(3,7**5,0,2**31 - 1,10000)
+n = np.arange(10000)
+fig,ax = pyplot.subplots()
+pyplot.title('Mersenne Prime')
+pyplot.scatter(n,m)
 pyplot.xlabel('x')
 pyplot.ylabel('y')
 
-dx.legend()
 
-pyplot.show()
+pyplot.show() 
