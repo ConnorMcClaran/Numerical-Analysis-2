c2d4be425a8ad4a2519e0986e3689dbc0a4370f0
Connor <connormcclaran@gmail.com>
2018-05-04 10:58:36 -0600

    graph function to minimize

--- Bananas.py
+++ Bananas.py
@@ -13,25 +13,30 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def f(x,y):
     return (1 - x**2) + 100*(((y - x**2))**2)
-
+def T(x,y):
+    return  np.sin((0.5*x**2) - (1/4.0)*y**2 +3)*np.cos(2*x + 1 - np.exp(y))
 y = np.linspace(0.0,2.0,num = 100)
 x = np.linspace(-2.0,2.0,num = 100)
-
+t = []
 z = []
 for i in range(100):
     z.append(f(x[i],y[i]))
+    t.append(T(x,y))
 
 
 fig  = pyplot.figure()
 
 banana = fig.add_subplot(111,projection = '3d')
 pyplot.title('f(x,y) on x[-2.0,2.0] y[0,2.0]')
-banana.plot(x,y,z)
+banana.plot(x,y,z,label = 'banana')
+#banana.plot(x,t,label = 'surface')
 banana.set_xlabel('x')
-
+pyplot.legend()
 banana.set_ylabel('y')
-banana.set_zlabel('z')
+banana.set_zlabel('f(x,y)')
 pyplot.show()
+
+
 
 #Minimize with Golden Section Search
 
